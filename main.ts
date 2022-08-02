9a66ccc10094d59e7f38a3e1941ae5170ed607ef
fyx <1101022351@qq.com>
2022-08-02 10:48:14 +0800

    大致完成

--- main.ts
+++ main.ts
@@ -1,89 +1,102 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { Metadata } from "./metadata";
 
-// Remember to rename these classes and interfaces!
-
+interface excludeKeyMap {
+	[name: string]: string;
+}
 interface MyPluginSettings {
-	mySetting: string;
+	excludeKeys: excludeKeyMap;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	excludeKeys: { "e3dea0f5-37f2-4d79-ae58-490af3228069": "position" },
+};
 
-export default class MyPlugin extends Plugin {
+export default class MetadataPrettierPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerMarkdownPostProcessor((element, context) => {
+			const excludeKeys = Object.values(this.settings.excludeKeys);
+			const allFrontmatter = context.frontmatter || {};
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			const filterFrontmatter = this.filterObj(
+				allFrontmatter,
+				excludeKeys
+			);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			const frontmatters: NodeListOf<HTMLElement> =
+				element.querySelectorAll(".frontmatter-container");
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			for (let index = 0; index < frontmatters.length; index++) {
+				const frontmatterEl = frontmatters.item(index);
+				if (!this.isEmpty(filterFrontmatter)) {
+					frontmatterEl.style = "display:block";
+					context.addChild(
+						new Metadata(frontmatterEl, filterFrontmatter)
+					);
+				} else {
+					frontmatterEl.style = "display:none";
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new MetadataPrettierSettings(this.app, this));
 	}
 
-	onunload() {
+	filterObj(obj: object, excludeKeys: string[]) {
+		const newObj = {};
+		Object.entries(obj).forEach(([key, value]) => {
+			if (!excludeKeys.includes(key)) {
+				newObj[key] = value;
+			}
+		});
+		return newObj;
+	}
+	isEmpty(obj: object) {
+		return Object.keys(obj).length === 0;
+	}
 
+	onunload() {}
+
+	getAllFrontmatter() {
+		const file = this.app.workspace.getActiveFile();
+		if (!file) {
+			return undefined;
+		}
+		const path = file.path;
+		const cache = this.app.metadataCache.getCache(path);
+		console.log(cache);
+
+		return cache?.frontmatter;
+	}
+	uuid(): string {
+		let d = Date.now();
+		if (
+			typeof performance !== "undefined" &&
+			typeof performance.now === "function"
+		) {
+			d += performance.now(); //use high-precision timer if available
+		}
+		return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+			/[xy]/g,
+			function (c) {
+				const r = (d + Math.random() * 16) % 16 | 0;
+				d = Math.floor(d / 16);
+				return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+			}
+		);
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,47 +104,83 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class MetadataPrettierSettings extends PluginSettingTab {
+	plugin: MetadataPrettierPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MetadataPrettierPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", {
+			text: "Meta prettier - Settings",
+		});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+			.setName("Add new exclude Key")
+			.addButton((cb) => {
+				cb.setButtonText("Add");
+				cb.setCta();
+				cb.onClick(() => {
+					this.addNewKey();
+				});
+			});
+
+		// init settingtab
+		for (const [uuid, language] of Object.entries(
+			this.plugin.settings.excludeKeys
+		)) {
+			const setting = new Setting(containerEl);
+			setting
+				.setName("Key")
+				.setDesc("")
+				.addText((text) =>
+					text
+						.setPlaceholder("add new key")
+						.setValue(language)
+						.onChange(async (value) => {
+							this.plugin.settings.excludeKeys[uuid] = value;
+							await this.plugin.saveSettings();
+						})
+				);
+			this.addExtraButton(setting, uuid);
+		}
+	}
+
+	addNewKey() {
+		const setting = new Setting(this.containerEl);
+		const uuid = this.plugin.uuid();
+
+		setting
+			.setName("key")
+			.setDesc("")
+			.addText((text) =>
+				text.setPlaceholder("add new key").onChange(async (value) => {
+					this.plugin.settings.excludeKeys[uuid] = value;
 					await this.plugin.saveSettings();
-				}));
+				})
+			);
+
+		this.addExtraButton(setting, uuid);
+	}
+
+	addExtraButton(setting: Setting, uuid: string) {
+		setting.addExtraButton((cb) => {
+			cb.setIcon("cross");
+			cb.setTooltip("Remove");
+			cb.onClick(() => {
+				delete this.plugin.settings.excludeKeys[uuid];
+				this.plugin.saveSettings();
+				(this.plugin.app as any).commands.removeCommand(
+					`${this.plugin.manifest.id}:${uuid}`
+				);
+				setting.settingEl.hide();
+			});
+		});
 	}
 }
---
+++ metadata.ts
@@ -0,0 +1,93 @@
+import { MarkdownRenderChild } from "obsidian";
+
+export class Metadata extends MarkdownRenderChild {
+	allFrontmatter: object;
+
+	constructor(containerEl: HTMLElement, allFrontmatter: object) {
+		super(containerEl);
+
+		this.allFrontmatter = allFrontmatter;
+	}
+
+	onload() {
+		const firstChild = this.containerEl.firstChild;
+		const element = this.containerEl;
+		while (element.firstChild) {
+			element.removeChild(element.firstChild);
+		}
+		console.log("firstChild", firstChild);
+
+		const containerEl = this.containerEl.createEl("div", {
+			cls: "frontmatter-section-container",
+		});
+
+		Object.entries(this.allFrontmatter).forEach(([key, value]) => {
+			let keyEl: HTMLElement;
+			let valueEl: HTMLElement;
+			if (key === "tags") {
+				keyEl = this.containerEl.createEl("div", {
+					cls: "frontmatter-section-label frontmatter-section",
+					text: key,
+				});
+				valueEl = this.containerEl.createEl("div", {
+					cls: "frontmatter-section-value frontmatter-section frontmatter-section-tags",
+				});
+
+				value.forEach((tag: string) => {
+					const tagEl = this.containerEl.createEl("a", {
+						text: `#${tag}`,
+						cls: "tag",
+						attr: { rel: "noopener", target: "_blank" },
+						href: `#${tag}`,
+					});
+					valueEl.append(tagEl);
+				});
+			} else {
+				keyEl = this.containerEl.createEl("div", {
+					cls: "frontmatter-section-label frontmatter-section",
+					text: key,
+				});
+				if (value.startsWith("http")) {
+					valueEl = this.containerEl.createEl("div", {
+						cls: "frontmatter-section-value frontmatter-section",
+					});
+
+					const aEl = this.containerEl.createEl("a", {
+						text: value,
+						cls: "external-link",
+						attr: { rel: "noopener", target: "_blank" },
+						href: value,
+					});
+					valueEl.append(aEl);
+				} else {
+					valueEl = this.containerEl.createEl("div", {
+						cls: "frontmatter-section-value frontmatter-section",
+						text: value,
+					});
+					valueEl.addEventListener("click", () => {
+						document.querySelector('[data-type="search"]').click();
+						this.search(`"${key}: ${value}"`);
+					});
+				}
+			}
+			containerEl.append(keyEl, valueEl);
+		});
+
+		this.containerEl.append(firstChild, containerEl);
+	}
+
+	search(value: string) {
+		console.log(
+			"el",
+			document.getElementsByClassName("search-input-container")[0]
+		);
+
+		const t = document
+			.getElementsByClassName("search-input-container")[0]
+			.getElementsByTagName("input")[0];
+		const evt = document.createEvent("HTMLEvents");
+		evt.initEvent("input", true, true);
+		t.value = value;
+		t.dispatchEvent(evt);
+	}
+}
